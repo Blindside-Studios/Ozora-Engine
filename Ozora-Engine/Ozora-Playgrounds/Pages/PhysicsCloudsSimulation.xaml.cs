74021279430127a90ba51b3ca98ecf82ab205f33
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-06-07 18:29:53 +0200

    Cloud generation works as intended
    In Ozora Playgrounds, Cloud particles are represented by half-transparent circles.

--- Ozora-Engine/Ozora-Playgrounds/Pages/PhysicsCloudsSimulation.xaml.cs
+++ Ozora-Engine/Ozora-Playgrounds/Pages/PhysicsCloudsSimulation.xaml.cs
@@ -12,20 +12,49 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Ozora;
+using System.Numerics;
+using System.Drawing;
 
 namespace Ozora_Playgrounds.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PhysicsCloudsSimulation : Page
     {
         public PhysicsCloudsSimulation()
         {
             this.InitializeComponent();
+
+            // make sure to wait for the CloudsGrid to be loaded so it has an actual width that can be used to compute the cloud distribution
+            CloudsGrid.Loaded += CloudsGrid_Loaded;
+        }
+
+        private void CloudsGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Ozora.Initializer initializer = new();
+            Ozora.CloudSettings settings = new()
+            {
+                AreaWidth = CloudsGrid.ActualWidth,
+                AreaHeight = CloudsGrid.ActualHeight,
+                ImageWidth = 50,
+                ImageHeight = 50,
+                DensityModifier = 4
+            };
+            System.Numerics.Vector3[] _vectorsList = initializer.GenerateCloudPositions(settings);
+
+            foreach (Vector3 position in _vectorsList)
+            {
+                Microsoft.UI.Xaml.Shapes.Rectangle rect = new();
+                rect.Height = 50;
+                rect.Width = 50;
+                rect.RadiusX = 25;
+                rect.RadiusY = 25;
+                rect.HorizontalAlignment = HorizontalAlignment.Left;
+                rect.VerticalAlignment = VerticalAlignment.Top;
+                rect.Translation = position;
+                rect.Fill = new SolidColorBrush(Microsoft.UI.Colors.White);
+                CloudsGrid.Children.Add(rect);
+                rect.Opacity = 0.5;
+            }
         }
     }
 }
